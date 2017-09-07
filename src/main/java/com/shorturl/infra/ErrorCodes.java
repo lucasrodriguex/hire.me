69b22af73613f147111ee87a2a3564b674d80089
Lucas Rodrigues <lucasrdx@outlook.com>
2017-09-07 15:45:08 -0300

    Melhorando nome das constantes do erros para melhor legibilidade do codigo

--- src/main/java/com/shorturl/infra/ErrorCodes.java
+++ src/main/java/com/shorturl/infra/ErrorCodes.java
@@ -1,13 +1,13 @@
 package com.shorturl.infra;
 
 public class ErrorCodes {
-	public static final String ERROR_001_CODE = "001";
-	public static final String ERROR_002_CODE = "002";
-	public static final String ERROR_003_CODE = "003";
-	public static final String ERROR_004_CODE = "004";
+	public static final String CUSTOM_LABEL_ALREADY_EXISTS_ERROR_CODE = "001";
+	public static final String INVALID_URL_FORMAT_ERROR_CODE = "002";
+	public static final String SHORTENED_URL_NOT_FOUND_ERROR_CODE = "003";
+	public static final String UNINFORMED_LABEL_ERROR_CODE = "004";
 
-	public static final String ERROR_001_DESCRIPTION = "CUSTOM LABEL ALREADY EXISTS";
-	public static final String ERROR_002_DESCRIPTION = "INVALID URL FORMAT";
-	public static final String ERROR_003_DESCRIPTION = "SHORTENED URL NOT FOUND";
-	public static final String ERROR_004_DESCRIPTION = "UNINFORMED URL";
+	public static final String CUSTOM_LABEL_ALREADY_EXISTS_ERROR_DESCRIPTION = "CUSTOM LABEL ALREADY EXISTS";
+	public static final String INVALID_URL_FORMAT_ERROR_DESCRIPTION = "INVALID URL FORMAT";
+	public static final String SHORTENED_URL_NOT_FOUND_ERROR_DESCRIPTION = "SHORTENED URL NOT FOUND";
+	public static final String UNINFORMED_LABEL_ERROR_DESCRIPTION = "UNINFORMED LABEL";
 }
