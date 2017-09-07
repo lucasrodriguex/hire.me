25a74e4b11c64ab331685082b5dc156d81abc0a2
Lucas Rodrigues <lucasrdx@outlook.com>
2017-09-07 17:19:34 -0300

    Criando testes unitarios para as 10 urls mais visitadas

--- src/test/java/com/shorturl/ShorturlApplicationTests.java
+++ src/test/java/com/shorturl/ShorturlApplicationTests.java
@@ -9,17 +9,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -46,14 +45,23 @@ public class ShorturlApplicationTests {
 	private MockMvc mockMvc;
 	
 	private static final String DEFAULT_URL = "http://bemobi.com.br";
-	private static final String DEFAULT_LABEL = "jedi";
-	private static final String INVALID_URL = "Test://bemobi.com.br";
 	private static final String DEFAULT_PATH = "/shorturl";
+	private static final String DEFAULT_LABEL = "jedi";
+	private static final String DEFAULT_LABEL_URL_PATH = "/u/jedi";
+	
+	private static final String INVALID_URL = "Test://bemobi.com.br";
+	private static final String TOP_TEN_VIEWS_PATH = "/shorturl/topTenViews";
+	
 	
 	@Before
 	public void setup() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 		repository.deleteByShortUrlLabel(DEFAULT_LABEL);
+	}
+	
+	@After
+	public void after() {
+		repository.deleteByShortUrlLabel(service.getLabel());
 	}
 	
 	/**
@@ -119,9 +127,7 @@ public class ShorturlApplicationTests {
 	
 	@Test
 	public void shouldReturnBadRequestWithNullLabelTest() throws Exception {
-		String label = null;
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(DEFAULT_PATH).param("label", label);
-		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(DEFAULT_PATH);
 		mockMvc.perform(requestBuilder).andExpect(status().isBadRequest())
 		.andExpect(jsonPath("$.label").value(""))
 		.andExpect(jsonPath("$.error_code").value(ErrorCodes.UNINFORMED_LABEL_ERROR_CODE))
@@ -135,11 +141,11 @@ public class ShorturlApplicationTests {
 															.param("url", DEFAULT_URL)
 															.param("custom_label", DEFAULT_LABEL);
 		mockMvc.perform(requestBuilder).andExpect(status().isCreated())
-		.andExpect(jsonPath("$.label").value(service.getLabel()))
+		.andExpect(jsonPath("$.label").value(DEFAULT_LABEL))
 		.andExpect(jsonPath("$.originalUrl").value(DEFAULT_URL));
 		
 		requestBuilder = MockMvcRequestBuilders.get(DEFAULT_PATH).param("label", DEFAULT_LABEL);
-		mockMvc.perform(requestBuilder).andExpect(status().isCreated())
+		mockMvc.perform(requestBuilder).andExpect(status().isOk())
 		.andExpect(jsonPath("$.shortUrlLabel").value(DEFAULT_LABEL))
 		.andExpect(jsonPath("$.originalUrl").value(DEFAULT_URL))
 		.andExpect(jsonPath("$.views").value(0));
@@ -153,20 +159,64 @@ public class ShorturlApplicationTests {
 	public void shouldRedirectToCreatedLabelTest() throws Exception {
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.post(DEFAULT_PATH).param("url", DEFAULT_URL)
 				.param("custom_label", DEFAULT_LABEL);
-		mockMvc.perform(requestBuilder);
+		mockMvc.perform(requestBuilder).andExpect(status().isCreated())
+		.andExpect(jsonPath("$.label").value(DEFAULT_LABEL))
+		.andExpect(jsonPath("$.originalUrl").value(DEFAULT_URL));
 		
-		requestBuilder = MockMvcRequestBuilders.get("/"+DEFAULT_LABEL);
+		requestBuilder = MockMvcRequestBuilders.get(DEFAULT_LABEL_URL_PATH);
 		mockMvc.perform(requestBuilder).andExpect(status().isFound()).andExpect(redirectedUrl(DEFAULT_URL));
 	}
 	
 	@Test
 	public void shouldReturnNotFoundIfShortUrlNotExistsTest() throws Exception {
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/asdf");
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/u/asdf");
 		mockMvc.perform(requestBuilder).andExpect(status().isNotFound())
 					.andExpect(jsonPath("$.label").value("asdf"))
 					.andExpect(jsonPath("$.error_code").value(ErrorCodes.SHORTENED_URL_NOT_FOUND_ERROR_CODE))
 					.andExpect(jsonPath("$.description").value(ErrorCodes.SHORTENED_URL_NOT_FOUND_ERROR_DESCRIPTION));
 	}
+	
+	
+	/** 
+	 * TOP TEN TEST
+	 */
+	
+	@Test
+	public void shouldReturnTopTenTest() throws Exception {
+		String label = "";
+		RequestBuilder requestBuilder;
+		//create 20 urls with custom label
+		for (int i = 0; i < 20; i++) {
+			label = DEFAULT_LABEL+i;
+			requestBuilder = MockMvcRequestBuilders.post(DEFAULT_PATH).param("url", DEFAULT_URL)
+					.param("custom_label", label);
+			mockMvc.perform(requestBuilder).andExpect(status().isCreated())
+			.andExpect(jsonPath("$.label").value(label))
+			.andExpect(jsonPath("$.originalUrl").value(DEFAULT_URL));
+		}
+		
+		Random random = new Random();
+		//access 10 urls
+		for (int i = 0; i < 10; i++) {
+			int randomNumber = random.nextInt((15 - 1)) + 1;
+			for(int j = 0; j < randomNumber; j++) //visit urls
+				label = DEFAULT_LABEL_URL_PATH+i;
+				requestBuilder = MockMvcRequestBuilders.get(label);
+				mockMvc.perform(requestBuilder).andExpect(status().isFound()).andExpect(redirectedUrl(DEFAULT_URL));
+		}
+		
+		requestBuilder = MockMvcRequestBuilders.get(TOP_TEN_VIEWS_PATH);
+		mockMvc.perform(requestBuilder).andExpect(status().isOk())
+		.andExpect(jsonPath("$", org.hamcrest.Matchers.hasSize(10)));
+		
+		//remove created urls
+		for (int i = 0; i < 20; i++) {
+			label = DEFAULT_LABEL+i;
+			repository.deleteByShortUrlLabel(label);
+		}
+	}
+	
+	
 	
 	@Test
 	public void shouldGenerateUniqueHashesWithoutFailTest() {
