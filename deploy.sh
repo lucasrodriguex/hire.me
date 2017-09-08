75e4e2004572271cc136dd90f331ae0026750e44
Lucas Rodrigues <lucasrdx@outlook.com>
2017-09-08 00:59:14 -0300

    Criando deploy pelo docker

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,25 @@
 #git clone https://github.com/lucasrodriguex/hire.me.git
 #cd hire.me
 
-docker run -p 3306:3306 --name mysql-dev -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=shorturl -e MYSQL_USER=root -e MYSQL_PASSWORD=root -d mysql:5.6
-docker start mysql-dev
+docker run -p 3306:3306 -d \
+    --name mysql-dev \
+    -e MYSQL_ROOT_PASSWORD=root \
+    -e MYSQL_DATABASE=shorturl \
+    -e MYSQL_USER=shorturl \
+    -e MYSQL_PASSWORD=shorturl \
+    mysql:5.6
 
 #test and generate package
 mvn clean package docker:build
 
 #application on!
-docker run --name shorturl --link mysql-dev:mysql -p 8080:8080 -d shorturl/shorturl
-docker start shorturl
+docker run -d \
+    --name shorturl \
+    --link mysql-dev:mysql \
+    -p 8080:8080 \
+    -e DATABASE_HOST=mysql \
+    -e DATABASE_PORT=3306 \
+    -e DATABASE_NAME=shorturl \
+    -e DATABASE_USER=shorturl \
+    -e DATABASE_PASSWORD=shorturl \
+    shorturl/shorturl
--- src/main/java/com/shorturl/infra/ErrorCodes.java
+++ src/main/java/com/shorturl/infra/ErrorCodes.java
@@ -6,8 +6,8 @@ public class ErrorCodes {
 	public static final String SHORTENED_URL_NOT_FOUND_ERROR_CODE = "003";
 	public static final String UNINFORMED_LABEL_ERROR_CODE = "004";
 
-	public static final String CUSTOM_LABEL_ALREADY_EXISTS_ERROR_DESCRIPTION = "CUSTOM LABEL ALREADY EXISTS";
-	public static final String INVALID_URL_FORMAT_ERROR_DESCRIPTION = "INVALID URL FORMAT";
-	public static final String SHORTENED_URL_NOT_FOUND_ERROR_DESCRIPTION = "SHORTENED URL NOT FOUND";
-	public static final String UNINFORMED_LABEL_ERROR_DESCRIPTION = "UNINFORMED LABEL";
+	public static final String CUSTOM_LABEL_ALREADY_EXISTS_ERROR_DESCRIPTION = "Custom label already exists";
+	public static final String INVALID_URL_FORMAT_ERROR_DESCRIPTION = "Invalid URL format";
+	public static final String SHORTENED_URL_NOT_FOUND_ERROR_DESCRIPTION = "Shortened Url not found";
+	public static final String UNINFORMED_LABEL_ERROR_DESCRIPTION = "Uninformed label";
 }
